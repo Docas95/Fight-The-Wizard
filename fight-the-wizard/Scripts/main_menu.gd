8353f2dfecc915539e53698f8efe58f2a90b85b7
Docas95 <dockitas152@gmail.com>
2024-10-08 19:20:35 +0100

    Implemented zone changer.

--- fight-the-wizard/Scripts/main_menu.gd
+++ fight-the-wizard/Scripts/main_menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://Scenes/tutorial_zone.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Zones/tutorial_zone.tscn")
 	
 func _on_controls_pressed():
 	pass
---
+++ fight-the-wizard/Scripts/zone_changer.gd
@@ -0,0 +1,8 @@
+extends Area2D
+
+@export_file var new_zone
+
+
+
+func _on_body_entered(body):
+	get_tree().change_scene_to_file(new_zone)
